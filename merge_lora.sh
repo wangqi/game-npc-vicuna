c77518bf714a5e0ef95a7a811b6a17f6643623b5
Qi Wang <to.wangqi@gmail.com>
2023-05-05 19:35:58 -0400

    fix the model  script in merge_lora.sh

--- merge_lora.sh
+++ merge_lora.sh
@@ -4,7 +4,7 @@ BASE_MODEL="${1:-"huggyllama/llama-7b"}"
 #LORA_WEIGHT="${2:-"models/chinese_llama_plus_lora_7b,models/chinese_alpaca_plus_lora_7b,Facico/Chinese-Vicuna-lora-7b-3epoch-belle-and-guanaco"}"
 #LORA_WEIGHT="${2:-"ziqingyang/chinese-llama-plus-lora-7b,ziqingyang/chinese-alpaca-plus-lora-7b,Facico/Chinese-Vicuna-lora-7b-3epoch-belle-and-guanaco"}"
 LORA_WEIGHT="${2:-"ziqingyang/chinese-llama-plus-lora-7b,ziqingyang/chinese-alpaca-plus-lora-7b"}"
-MODEL_PATH="${3:-"models/huggyllama_chinese-alpaca-plus-lora-7b-test"}"
+MODEL_PATH="${3:-"models/huggyllama_chinese-alpaca-plus-lora-7b"}"
 OUT_TYPE="f16"
 
 mkdir -p $MODEL_PATH
